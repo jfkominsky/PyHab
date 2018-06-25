1527d834a8c7a9b793e8e8a1ef80e3ae38ff0ab6
jfkominsky <kominskj@gmail.com>
2018-06-25 11:16:00 -0400

    Fixed all RTD themes for local docs and in general
    Now the documentation is both functional AND aesthetically appealing.

--- docs/conf.py
+++ docs/conf.py
@@ -30,6 +30,11 @@ for mod_name in MOCK_MODULES:
 sys.path.insert(0, os.path.abspath('../'))
 
 
+on_rtd = os.environ.get('READTHEDOCS') == 'True'  # For RTD vs. not.
+if on_rtd:
+    html_theme = 'default'
+else:
+    html_theme = 'sphinx_rtd_theme'
 # -- Project information -----------------------------------------------------
 
 project = u'PyHab'
@@ -89,7 +94,7 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+#html_theme = 'alabaster'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
