cb15599e8a4ad1c7ba2bd6bf047d4b6785e0d67c
jfkominsky <jonathan.f.kominsky@gmail.com>
2020-02-27 13:20:55 -0500

    Working directory fix, for now
    BACK COMPATIBILITY ISSUE
    
    Starting in PsychoPy 2020.1.0 the new runner window seems to move the working directory, which is a huge problem for PsychoPy. This manually drags the working directory back to the script location, which is how it behaved prior to 2020.1.0. Unfortunately, these extra lines of code will be needed for every existing PyHab experiment if they use 2020.1.0 or later, so it breaks back compatibility to a degree. hopefully a future change to PsychoPy renders them unnecessary.

--- NewPyHabProject.py
+++ NewPyHabProject.py
@@ -2,6 +2,8 @@
 #Simply hit the green running-silhouette button to begin! 
 #(You can also press CTRL-R or Command-R)
 from PyHab import PyHabBuilder as PB
+import os
+os.chdir(os.path.dirname(os.path.realpath(__file__))) # require for PsychoPy3 2020.1.0 and later (for now)
 
 builder = PB.PyHabBuilder()
 builder.run()
--- PyHab/PyHab Launcher.py
+++ PyHab/PyHab Launcher.py
@@ -4,6 +4,7 @@ from PyHab import PyHabClassPL as PHL
 from PyHab import PyHabClassHPP as PHPP
 from PyHab import PyHabBuilder as PB
 import csv, os
+os.chdir(os.path.dirname(os.path.realpath(__file__))) # require for PsychoPy3 2020.1.0 and later (for now)
 
 setName = 'DUMMY FILENAME'
 
