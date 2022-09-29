9741beef53516a877920bebb5889050317e09842
jfkominsky <kominskj@gmail.com>
2022-09-29 11:08:24 +0200

    Renaming files and folders
    Just to reduce any chance of confusion.

--- PyHabOnlineEdition/NewPyHabOnlineProject.py
+++ PyHabOnlineEdition/NewPyHabOnlineProject.py
@@ -1,7 +1,7 @@
 #For making PyHab studies from a blank slate. 
 #Simply hit the green running-silhouette button to begin! 
 #(You can also press CTRL-R or Command-R)
-from PyHab import PyHabBuilder as PB
+from PyHabOnline import PyHabBuilder as PB
 import os
 #Required for PsychoPy 2020.1.0 and 2020.1.1
 os.chdir(os.path.dirname(os.path.realpath(__file__))) 
